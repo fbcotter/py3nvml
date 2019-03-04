6bc82e9a3e6bdfe1d3a5f197a52031f9d3cf0df3
Fergal Cotter <fbcotter90@gmail.com>
2019-03-04 18:14:24 +0000

    Updated version number

--- py3nvml/__init__.py
+++ py3nvml/__init__.py
@@ -2,8 +2,7 @@ from __future__ import absolute_import
 
 from py3nvml import py3nvml
 from py3nvml import nvidia_smi
-from py3nvml.utils import grab_gpus
-from py3nvml.utils import get_free_gpus
+from py3nvml.utils import grab_gpus, get_free_gpus, get_num_procs
 
-__all__ = ['py3nvml', 'nvidia_smi', 'grab_gpus', 'get_free_gpus']
-__version__ = "0.2.2"
+__all__ = ['py3nvml', 'nvidia_smi', 'grab_gpus', 'get_free_gpus', 'get_num_procs']
+__version__ = "0.2.3"
