f56a4b351d92d506f386679c679da8b1b4e322b8
Fergal Cotter <fbcotter90@gmail.com>
2021-11-22 14:24:52 +0000

    bumped version number

--- py3nvml/__init__.py
+++ py3nvml/__init__.py
@@ -5,4 +5,4 @@ from py3nvml import nvidia_smi
 from py3nvml.utils import grab_gpus, get_free_gpus, get_num_procs
 
 __all__ = ['py3nvml', 'nvidia_smi', 'grab_gpus', 'get_free_gpus', 'get_num_procs']
-__version__ = "0.2.6"
+__version__ = "0.2.7"
